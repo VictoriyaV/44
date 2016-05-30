848be9ea8fc78a2d43abd19e85fa14e0ddfef46c
VictoriyaV <vikav02@mail.ru>
2016-05-30 09:49:34 +0300

    Add files via upload

--- web/js/lr4.js
+++ web/js/lr4.js
@@ -1,9 +1,17 @@
-var DT = new Date,
-        Ye = DT.getFullYear(),
-        Mo = DT.getMonth(),
-        Da = DT.getDate(),
-        Cd = new Date(Ye, Mo, Da).getTime();
-Ye = (Da < 23 && Mo < 7) ? Ye : Ye + 1;
-var Fd = new Date(Ye, 7, 23).getTime();
-var R = Math.ceil((Fd - Cd) / 8.64e7); // 8.64e7 - число милисекунд в году
-alert('До ближайшего 23 августа ' + ' - ' + R + ' дн.');
+﻿var today = new Date();
+ 
+ 
+var SEP = new Date("September 1, 2016");
+ 
+var time = (SEP.getTime() - today.getTime());
+ 
+ 
+var e_days = time / 86400000;
+ var days = Math.floor(e_days);
+  
+var  ned = Math.floor(days/7);
+ 
+alert("До 1 сенября осталось "+ned+" недель");
+
+
+ 
